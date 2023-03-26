6154bc05edb47832bc7add18fad47cfbe990f7ff
shes50103 <shes50103@gmail.com>
2023-03-26 17:49:27 +0100

    Add tickets_total attribute

--- app/resources/api/order_resource.rb
+++ app/resources/api/order_resource.rb
@@ -1,6 +1,6 @@
 module Api
   class OrderResource < JSONAPI::Resource
-    attributes :total, :user_id
+    attributes :total, :user_id, :tickets_total
     has_many :tickets
 
     paginator :offset
@@ -8,5 +8,9 @@ module Api
     filter :user, apply: ->(records, value, _options) {
       records.where(user_id: value)
     }
+
+    def tickets_total
+      @model.tickets.sum(:total)
+    end
   end
 end
